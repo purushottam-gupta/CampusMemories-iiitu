16f67f132654ca370b5b97d90b9ad66febe8112b
Purushottam Gupta <guptapurushottam123@gmail.com>
2022-04-08 18:37:04 +0530

    make it better

---
+++ client/build/precache-manifest.34d2168395200add14e7582edb2a3137.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3d0c1dda8ed931ebe0faad3d2b78780",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc7a1aaffb990ebacc21",
+    "url": "/static/css/main.582cd850.chunk.css"
+  },
+  {
+    "revision": "db4e2c6de4e441f0de9a",
+    "url": "/static/js/2.7b258b4e.chunk.js"
+  },
+  {
+    "revision": "c06d497d6716e84111c2fd9253bcfc02",
+    "url": "/static/js/2.7b258b4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc7a1aaffb990ebacc21",
+    "url": "/static/js/main.6efc82ef.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5d4aee407c522936f988e89ebe0348c7",
+    "url": "/static/media/logo-iiitu.5d4aee40.gif"
+  },
+  {
+    "revision": "2b89c5fae2d163d5a126a519646883fa",
+    "url": "/static/media/memoriesText.2b89c5fa.png"
+  }
+]);
